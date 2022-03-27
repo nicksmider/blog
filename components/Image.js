1b359e98b5a4145b0404cc6f57e4d09b97469f5e
Nick Smider <nicksmider@gmail.com>
2022-03-26 21:22:09 -0400

    Add prefix for images

--- components/Image.js
+++ components/Image.js
@@ -1,8 +1,9 @@
+import { prefix } from '@/lib/prefix'
 import NextImage from 'next/image'
 
 // opt-out of image optimization, no-op
 const imageLoader = ({ src }) => {
-  return src
+  return `${prefix}${src}`
 }
 // eslint-disable-next-line jsx-a11y/alt-text
 const Image = ({ ...rest }) => <NextImage loader={imageLoader} {...rest} />
